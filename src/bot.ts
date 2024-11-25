2a34f90054ae005ba708e287e788160ddc1893b7
Смирнов Алексей Юрьевич <AYurySmirnov@sberbank.ru>
2024-11-25 21:00:50 +0300

    feat: simplex bot

---
+++ src/bot.ts
@@ -0,0 +1,20 @@
+
+import {Bot} from "grammy";
+
+const bot = new Bot(process.env.TELEGRAM_TOKEN || "");
+
+const introductionMessage = `Hello! I'm a Telegram bot.
+I'm powered by Cyclic, the next-generation serverless computing platform.
+
+<b>Commands</b>
+/yo - Be greeted by me
+/effect [text] - Show a keyboard to apply text effects to [text]`;
+
+const replyWithIntro = (ctx: any) =>
+    ctx.reply(introductionMessage, {
+        parse_mode: "HTML",
+    });
+
+bot.on("message", replyWithIntro);
+
+bot.start();
