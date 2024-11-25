5019b8ac3c1500643a25f38d706fff3cabe807d2
Смирнов Алексей Юрьевич <AYurySmirnov@sberbank.ru>
2024-11-25 21:38:18 +0300

    feat: можно отправлять сообщения

--- src/bot.ts
+++ src/bot.ts
@@ -3,17 +3,10 @@ import {Bot} from "grammy";
 
 const bot = new Bot(process.env.TELEGRAM_TOKEN || "");
 
-const introductionMessage = `Hello! I'm a Telegram bot.
-I'm powered by Cyclic, the next-generation serverless computing platform.
-
-<b>Commands</b>
-/yo - Be greeted by me
-/effect [text] - Show a keyboard to apply text effects to [text]`;
+const introductionMessage = "test"
 
 const replyWithIntro = (ctx: any) =>
-    ctx.reply(introductionMessage, {
-        parse_mode: "HTML",
-    });
+    ctx.reply(introductionMessage);
 
 bot.on("message", replyWithIntro);
 
